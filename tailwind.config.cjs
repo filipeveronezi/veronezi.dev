c976c25358374381c6d315f9b49ecdccc4f10205
Filipe Veronezi <filipeseidi@hotmail.com>
2023-02-01 00:37:36 -0300

    ✨ update fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -6,6 +9,9 @@ module.exports = {
       backgroundImage: {
         'grainy-pattern': "url('/noise.svg')",
         'gradient-pattern': "url('/gradient.jpg')"
+      },
+      fontFamily: {
+        sans: ['Unbounded', ...defaultTheme.fontFamily.sans]
       }
     }
   },
