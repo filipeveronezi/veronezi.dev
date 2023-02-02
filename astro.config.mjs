92743f9f601c2fcbd17e43d78963930cd172de76
Filipe Veronezi <filipeseidi@hotmail.com>
2023-02-01 22:26:27 -0300

    👷 setup vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-})
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
+});
