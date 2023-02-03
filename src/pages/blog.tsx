d90cb9077cee0f7d00685ae69d26592d43725f57
Filipe Veronezi <filipeseidi@hotmail.com>
2023-02-03 16:07:08 -0300

    🔍 update seo meta tags

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,6 +5,20 @@ export default function Blog() {
     <>
       <Head>
         <title>F. Veronezi — Blog</title>
+        <meta name="title" content="Filipe Veronezi — Blog" />
+        <meta name="description" content="Sharing a bit of my journey as a web developer." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.veronezi.dev/blog" />
+        <meta property="og:title" content="Filipe Veronezi — Blog" />
+        <meta property="og:description" content="Sharing a bit of my journey as a web developer." />
+        <meta property="og:image" content="/thumbnail.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.veronezi.dev/blog" />
+        <meta property="twitter:title" content="Filipe Veronezi — Blog" />
+        <meta property="twitter:description" content="Sharing a bit of my journey as a web developer." />
+        <meta property="twitter:image" content="/thumbnail.png" />
       </Head>
       <main className="mx-auto flex w-full max-w-screen-sm flex-col gap-5 px-6 lg:px-0">
         <h1 className="text-4xl font-bold">Blog</h1>
