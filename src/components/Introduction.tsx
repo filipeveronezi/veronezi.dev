bf22a4f207d311d6e6ec40316d353b93e0a7de1b
Filipe Veronezi <filipeseidi@hotmail.com>
2023-04-21 23:17:01 -0300

    ✨ emojis on introduction text

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,9 +5,9 @@ export function Introduction() {
 
   return (
     <p className="group flex animate-fade-up flex-col text-lg leading-8 animation-delay-100">
-      <span>Hey, welcome to my place on the web.</span>
+      <span>Hey, welcome to 💫 my place on the web.</span>
       <span>I&apos;m a {age} y/o developer from 🇧🇷 Brazil.</span>
-      <span> I love to build beautiful and delightful user experiences, like this one.</span>
+      <span> I 💜 to build beautiful and delightful user experiences, like this one.</span>
     </p>
   )
 }
