a1331c14f96d45d1082ea6b63bc62a726667721b
Filipe Veronezi <filipeseidi@hotmail.com>
2023-10-27 21:19:50 -0300

    chore: remove all files

--- src/components/NoiseTexture.tsx
+++
@@ -1,5 +0,0 @@
-export function NoiseTexture() {
-  return (
-    <div className="mix bg-grainy-pattern pointer-events-none fixed z-50 h-full w-full opacity-70 mix-blend-soft-light"></div>
-  )
-}
--- src/pages/outstatic/[[...ost]].tsx
+++
@@ -1,6 +0,0 @@
-import { OstSSP, Outstatic } from 'outstatic'
-import 'outstatic/outstatic.css'
-
-export default Outstatic
-
-export const getServerSideProps = OstSSP
