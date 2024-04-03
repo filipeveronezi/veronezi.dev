a2f6cff2615b7383627403aab12d5c04860e1216
Filipe Veronezi <filipeseidi@hotmail.com>
2024-04-03 01:52:50 -0300

    chore: update 'about me' title

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'F. Veronezi — About Me',
+  description: 'Design Engineer. Building for the web.'
+}
 
 export default function AboutMe() {
   return (
