0b27a4c65cae5f14d201e5b895e05532cc464996
Filipe Veronezi <filipeseidi@hotmail.com>
2024-05-15 07:31:33 -0300

    feat(craft): metadata

--- src/app/craft/page.tsx
+++ src/app/craft/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'F. Veronezi — Craft',
+  description: 'Design Engineer. Building for the web.'
+}
 
 export default function Craft() {
   return (
