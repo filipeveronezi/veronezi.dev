bdeae1dfdf913f74bfd7cc94b9d99f8f9e9df7ed
Filipe Veronezi <filipeseidi@hotmail.com>
2026-05-20 12:34:44 -0300

    feat: signature

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,20 @@
+import { Signature } from "@/components/signature";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="mx-auto flex h-56 w-full max-w-2xl items-end justify-between px-2 pb-6">
+    <footer className="mx-auto flex h-56 w-full max-w-2xl items-end justify-between px-4 pb-12">
       <span />
-      <Link
-        href="/"
-        className="bg-linear-to-r from-zinc-500 via-zinc-800 to-zinc-400 bg-clip-text text-sm font-medium text-transparent"
-      >
-        veronezi.dev
-      </Link>
+      <div className="relative">
+        <Signature className="absolute inset-0 m-auto size-16 opacity-60" />
+        <div className="absolute inset-0 m-auto h-4 w-full bg-white blur-sm" />
+        <Link
+          href="/"
+          className="relative z-50 bg-linear-to-r from-zinc-600 via-zinc-800 to-zinc-500 bg-clip-text text-sm font-medium text-transparent"
+        >
+          veronezi.dev
+        </Link>
+      </div>
     </footer>
   );
 }
